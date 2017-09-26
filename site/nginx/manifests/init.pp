d05a8d4dbf3654e265d1034a49b16b253b729bd3
Greg Jewett <greg@ejewett.com>
2017-09-26 15:11:29 -0500

    lab 8.3 fixed typos, source correct files.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -14,10 +14,20 @@ class nginx {
     require => Package['nginx'],
     source => 'puppet:///modules/nginx/index.html',
   }
+  
+  file { "/var/www/index.html":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
 
   service { 'nginx':
     ensure => running,
     enable => true,
     subscribe => File['/etc/nginx/nginx.conf'],
   }
+  
 }
