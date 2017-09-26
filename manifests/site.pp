df32150ff13220bee38f7c5d7865889fa5fa98f8
jostomlin <32303747+jostomlin@users.noreply.github.com>
2017-09-26 12:46:06 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd'
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+  content => "Think before you type\n",
 }
