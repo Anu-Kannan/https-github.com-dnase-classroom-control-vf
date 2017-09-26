168051927afe0cf83bcc48f30c6bb3483d38887d
desjardy <sigatapoui@gmail.com>
2017-09-26 13:43:25 -0400

    adds managed file motd
    adding motd as managed file

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,11 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   notify {"tastytest":}
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Woot Woot tastytest\n",
+  }
 }
