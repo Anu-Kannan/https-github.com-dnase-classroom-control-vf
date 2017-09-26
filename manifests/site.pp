5808459d9f435440deda10cf945045537ab51662
jostomlin <32303747+jostomlin@users.noreply.github.com>
2017-09-26 13:18:34 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
   include role::classroom
   exec { 'lab 7.2':
     command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-    path  => '/bin',
+    path  => '/usr/local/bin',
     creates => '/etc/motd',
     }
 }
