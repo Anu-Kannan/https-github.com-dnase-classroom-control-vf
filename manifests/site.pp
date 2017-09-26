472438d7a509ab7612cdfbc8d124c0407113b9cb
jostomlin <32303747+jostomlin@users.noreply.github.com>
2017-09-26 12:52:29 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,9 +44,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0664',
-  content => "Yahoo!",
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    content => "Yahoo!",
 }
