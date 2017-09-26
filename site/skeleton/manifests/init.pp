080fc4bb1a21b7d65488e4aa0d82ec9e28f89d49
lancedawkins <32281682+lancedawkins@users.noreply.github.com>
2017-09-26 12:23:26 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,11 @@
+class skeleton
+  file { '/etc/skel':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    }
 
+  file { 'etc/skel/.bashrc':
+    ensure => file,
+    source => 'puppet:///modules/skeleton/bashrc'
