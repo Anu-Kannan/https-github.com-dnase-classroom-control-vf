aad1c5ddd6293c2442f4617c9262e60dbfa0cd91
mtrog <Mathew.Rogers1@gmail.com>
2017-09-27 10:10:01 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,34 @@
-class nginx { 
-  package { 'nginx_pack':
-    ensure => present,
-    name => 'nginx',
-  }
-  
+class nginx {
   File {
     owner => 'root',
     group => 'root',
     mode => '0644',
   }
-  
+  package { 'nginx':
+    ensure => present,
+  }  
   file { '/var/www':
     ensure => directory,
   }
-  
-  file { 'nginx_conf':
-    ensure => 'file',
-    path => '/etc/nginx/nginx.conf',
-    require => Package['nginx_pack'],
-    source => 'puppet:///modules/nginx/nginx.conf'
-  }
-  
-  file { 'nginx_default':
-    ensure => 'file',
-    path => '/etc/nginx/conf.d/default.conf',
-    require => Package['nginx_pack'],
-    source => 'puppet:///modules/nginx/default.conf',
-  }
-  
-  file { 'nginx_index':
-    ensure => 'file',
-    path => '/var/www/index.html',
+  file { '/var/www/index.html':
+    ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  
-  service { 'nginx_service':
-    ensure => 'running',
-    enable => 'true',    
-    subscribe => File['nginx_conf', 'nginx_default'],
-    name => 'nginx',
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
   }
 }
