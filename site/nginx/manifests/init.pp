aabf64fd92cc9ad45a1d48a7bb58fcb902c3b0a3
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 04:33:14 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,14 +1,34 @@
 class nginx{
 package{ 'nginx':
-   ensure=>installed
+   ensure=>present,
 }
-file{'root':
-   path=>'/var/www',
+file{'/var/www':
+   ensure => directory,
+   owner => 'root',
+   group => 'root',
+   mode => '0775',
 }
-file{'nginx.conf':
-   path=>'/etc/nginx/nginx.conf',
+file{'/etc/nginx/nginx.conf':
+   ensure=>file,
+   owner=>'root',
+   group=>'root',
+   mode=>'0664',
+   source=>'puppet:///nginx/nginx.conf',
+   require => Package['nginx'],
+   notify => Service['nginx'],
 }
-file{'default.conf':
-   path=>'/etc/nginx/conf.d/default.conf',
+file{'/etc/nginx/conf.d/default.conf':
+   ensure=>file,
+   owner=>'root',
+   group=>'root',
+   mode=>'0775',
+   source=>'puppet:///nginx/default.conf',
+}
+file{'index.html':
+   ensure=>file,
+   owner=>'root',
+   group=>'root',
+   mode=>'0775',
+   source=>'puppet:///nginx/index.html',
 }
 }
