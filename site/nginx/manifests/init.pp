ed9911cdcac1336b775c225de3ce6ebf49b9f27a
asmaaf <32303783+asmaaf@users.noreply.github.com>
2017-09-27 13:01:38 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,8 +1,12 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
     ensure => present,
-  }
-  
+  }  
   file { '/var/www':
     ensure => directory,
   }
@@ -10,7 +14,6 @@ class nginx {
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  
   file { 'nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
@@ -20,10 +23,9 @@ class nginx {
   file { 'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
-  }
-  
+  }  
   service { 'nginx':
     ensure => running,
     enable => true,
