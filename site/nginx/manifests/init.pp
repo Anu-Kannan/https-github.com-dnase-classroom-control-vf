f4703e7337a697909659431689410138de1f6c56
desjardy <sigatapoui@gmail.com>
2017-09-27 12:46:15 -0400

    code simplification

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,11 @@
 class nginx {
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nginx'],
+  }
+  
   package {'nginx':
     ensure  => present,
   }
@@ -9,28 +16,16 @@ class nginx {
   
   file {'/var/www/index.html':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   file {'/etc/nginx/nginx.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
     source  => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file {'/etc/nginx/conf.d/default.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
     source  => 'puppet:///modules/nginx/default.conf',
   }
   
