07de79a4c6c0d6697121efe7f15652ff0e31a3a9
desjardy <sigatapoui@gmail.com>
2017-09-27 14:34:05 -0400

    add case platforms

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,42 @@
 class nginx {
   $source = 'puppet:///modules/nginx/'
-  
+  case $facts['os']['name'] {
+    'redhat', 'centos': { 
+      $packname = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $servdir  = '/etc/nginx/conf.d'
+      $logsdir  = '/var/log/nginx'
+      $service  = 'nginx'
+      $runas    = 'nginx'
+    } # apply the RedHat class
+    'debian', 'ubuntu': { 
+      $packname = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $servdir  = '/etc/nginx/conf.d'
+      $logsdir  = '/var/log/nginx'
+      $service  = 'nginx'
+      $runas    = 'www-data'      
+    } # apply the Debian class
+    'windows'         : {
+      $packname = 'nginx-service'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $servdir  = 'C:/ProgramData/nginx/conf.d'
+      $logsdir  = 'C:/ProgramData/nginx/logs'
+      $service  = 'nginx'
+      $runas    = 'nobody'
+    } # apply the Windows class
+    default:  { fail("Unsupported OS: ${facts['os']['name']}") }
+  }
+    
   File {
     owner   => 'root',
     group   => 'root',
@@ -28,7 +64,7 @@ class nginx {
   
   file {'/etc/nginx/conf.d/default.conf':
     ensure  => file,
-    source  => "${source}default.conf",
+    content => epp('nginx/default.conf.epp', { docroot => $docroot })
     require => Package['nginx'],
   }
   
