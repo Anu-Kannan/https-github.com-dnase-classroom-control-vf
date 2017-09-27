7d862451b9d1c4b32a3120eabcecfdec318bd6a4
Jeff Schlags <jschlags@mac-ottawaapple.sei.cmu.edu>
2017-09-27 12:58:59 -0400

    lab 12.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,44 +1,35 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
-  }
-  
-  file { '/var/www':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
+  File {
     owner => 'root',
     group => 'root',
     mode => '0644',
   }
-  
+  package { 'nginx':
+    ensure => present,
+  }
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
   file { 'nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
-    owner => 'root',
-    group => 'root',
-    mode => '0644',    
   }
   file { 'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
-    owner => 'root',
-    group => 'root',
-    mode => '0644',    
   }
-  
   service { 'nginx':
     ensure => running,
     enable => true,
     subscribe => File['nginx.conf', 'default.conf'],
   }
 }
+
