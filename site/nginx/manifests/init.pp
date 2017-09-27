a81cffa64dcd173bd4ec6a4aa82a309f93dea9b2
Jeff Schlags <jschlags@mac-ottawaapple.sei.cmu.edu>
2017-09-27 12:54:43 -0400

    updated nginx manifest

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,10 +5,16 @@ class nginx {
   
   file { '/var/www':
     ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
   }
   file { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
   }
   
   file { 'nginx.conf':
@@ -16,12 +22,18 @@ class nginx {
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
+    owner => 'root',
+    group => 'root',
+    mode => '0644',    
   }
   file { 'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
+    owner => 'root',
+    group => 'root',
+    mode => '0644',    
   }
   
   service { 'nginx':
