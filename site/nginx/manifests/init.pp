5d61542d7aeb289a991792b4d44ac99b186a2608
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 12:31:40 -0400

    HW8.4
    added owner and group

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,20 +5,32 @@ class nginx {
   
   file { '/var/www':
     ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
   }
   file { '/var/www/index.html':
     ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
     ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
   file { 'default.conf':
     ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
