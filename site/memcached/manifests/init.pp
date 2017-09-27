772113171799aa922fe2cfe555518f75a368ec0b
drewvas <32267047+drewvas@users.noreply.github.com>
2017-09-27 10:29:40 -0400

    11.1

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -5,5 +5,11 @@ ensure => present,
 file {'etc/sysconfig/memcached':,
 ensure =>file,
 content => PORT='11211'\nUSER='memcached'\nMAXCONN='96'\nCACHESIZE='32'\nOPTIONS=''\n
+require => Package['memcached'], 
 }
-service {'memcached':
+service { 'memcached':    
+ensure => running,    
+enable => true,    
+subscribe => File['/etc/sysconfig/memcached'],  
+}
+}
