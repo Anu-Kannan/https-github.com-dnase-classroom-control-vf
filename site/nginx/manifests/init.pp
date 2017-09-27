47d4c0b4450cb0c8f1c8e420cf41fb9bf4d19037
Parker Wagnon <parkerw262@gmail.com>
2017-09-27 11:59:07 -0500

    clean up init.dd

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,40 @@
+class nginx {
+File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    }
+  
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+
+  }
+  
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+   
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
