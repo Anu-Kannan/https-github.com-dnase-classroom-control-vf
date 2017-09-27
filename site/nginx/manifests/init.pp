ccc2e61254489ef11da15140adc867c6124328d2
Greg Jewett <greg@ejewett.com>
2017-09-27 11:56:34 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,48 @@
-# ${modulepath}/nginx/manifests/init.pp
-
 class nginx {
-	package { 'nginx':
-    	ensure => present, }
+
+	File {
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
 	}
-  
+
+	package { 'nginx':
+		ensure => present,
+	}
+	
 	file { '/var/www':
 		ensure => directory,
 	}
-  
-  	file { "index.html":
-    	ensure => file,
-		path => '/var/www/index.html',
+	
+	file { '/var/www/index.html':
+		ensure => file,
 		source => 'puppet:///modules/nginx/index.html',
-    	require => Package['nginx'],
-  	}
-
+	}
+	
+	file { 'nginx.conf':
+		ensure => file,
+		path => '/etc/nginx/nginx.conf',
+		source => 'puppet:///modules/nginx/nginx.conf',
+		require => Package['nginx'],
+	}
+	
+	file { 'default.conf':
+		ensure => file,
+		path => '/etc/nginx/conf.d/default.conf',
+		source => 'puppet:///modules/nginx/default.conf',
+		require => Package['nginx'],
+	}
+	
 	file { 'puppet.png':
 		ensure => file,
-    	require => Package['nginx'],
+		require => Package['nginx'],
 		path => '/var/www/puppet.png',
-    	source => 'puppet:///modules/nginx/puppet.png',
-  	}
-
-	file { 'nginx.conf':
-    	ensure => file,
-		path => '/etc/nginx/nginx.conf',
-    	source => 'puppet:///modules/nginx/nginx.conf',
-    	require => Package['nginx'],
-	}
-
-	file { 'default.conf':
- 		ensure => file,
-		path => '/etc/nginx/conf.d/default.conf',
-    	source => 'puppet:///modules/nginx/nginx.conf',
-    	require => Package['nginx'],
+		source => 'puppet:///modules/nginx/puppet.png',
 	}
 	
 	service { 'nginx':
-    	ensure => running,
-    	enable => true,
- 		subscribe => File['nginx.conf', 'default.conf', 'puppet.png'],
-  	}
+		ensure => running,
+		enable => true,
+		subscribe => File['nginx.conf', 'default.conf'],
+	}
 }
