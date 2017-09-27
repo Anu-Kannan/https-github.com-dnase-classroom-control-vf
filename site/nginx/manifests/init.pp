a53ab398f8ffca5241b67aa6351779ddc49b5a69
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 15:06:50 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,6 @@
 class nginx {
-  File{
-      owner => 'root',
-      group => 'root',
-  }
-  package { 'nginx':
-    ensure => present,
-  }
-  
-  file { '/var/www':
-    ensure => directory,
-    mode => '0775',
-  }
-  file { '/var/www/index.html':
-    ensure => file,
-    mode => '0664',
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  
-  file { 'nginx.conf':
-    ensure => file,
-    mode => '0664',
-    path => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-  file { 'default.conf':
-    ensure => file,
-    mode => '0664',
-    path => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-  }
-  
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File['nginx.conf', 'default.conf'],
-  }
-}
-
-class nginx {
+$service =
+$port = 80
 case $facts['os']['family'] {
 'redhat','debian' : {
 $package = 'nginx'
