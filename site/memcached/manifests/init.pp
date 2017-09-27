94563888de84f91ed2396aeaafc4e7413242596e
drewvas <32267047+drewvas@users.noreply.github.com>
2017-09-27 10:26:23 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,9 @@
+class memcached {
+package {'memcached':
+ensure => present,
+}
+file {'etc/sysconfig/memcached':,
+ensure =>file,
+content => PORT='11211'\nUSER='memcached'\nMAXCONN='96'\nCACHESIZE='32'\nOPTIONS=''\n
+}
+service {'memcached':
