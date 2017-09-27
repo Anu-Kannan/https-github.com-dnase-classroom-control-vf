2b71cbecf6a4174dd9badd85cd14b3d86ef12dc6
mtrog <Mathew.Rogers1@gmail.com>
2017-09-27 09:50:24 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,26 +3,37 @@ class nginx {
     ensure => present,
     name => 'nginx',
   }
+  
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+  
   file { '/var/www':
     ensure => directory,
   }
+  
   file { 'nginx_conf':
     ensure => 'file',
     path => '/etc/nginx/nginx.conf',
     require => Package['nginx_pack'],
     source => 'puppet:///modules/nginx/nginx.conf'
   }
+  
   file { 'nginx_default':
     ensure => 'file',
     path => '/etc/nginx/conf.d/default.conf',
     require => Package['nginx_pack'],
     source => 'puppet:///modules/nginx/default.conf',
   }
+  
   file { 'nginx_index':
     ensure => 'file',
     path => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
+  
   service { 'nginx_service':
     ensure => 'running',
     enable => 'true',    
