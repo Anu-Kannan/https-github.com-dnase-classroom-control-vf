215b5d2bfa646b2e541a093c45029a9e0074d1b8
Greg Jewett <greg@ejewett.com>
2017-09-27 08:26:01 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,49 +1,45 @@
 # ${modulepath}/nginx/manifests/init.pp
 
 class nginx {
-  package { 'nginx':
-    ensure => present, }
-    #file { '/etc/nginx/nginx.conf':
-  }
+	package { 'nginx':
+    	ensure => present, }
+	}
   
-  file { '/var/www':
-    ensure => directory,
-  }
+	file { '/var/www':
+		ensure => directory,
+	}
   
-  file { "index.html":
-    ensure => file,
+  	file { "index.html":
+    	ensure => file,
 		path => '/var/www/index.html',
-    source => 'puppet:///modules/nginx/index.html',
-    require => Package['nginx'],
-  }
+		source => 'puppet:///modules/nginx/index.html',
+    	require => Package['nginx'],
+  	}
 
-  file { 'puppet.png':
-    ensure => file,
-    require => Package['nginx'],
+	file { 'puppet.png':
+		ensure => file,
+    	require => Package['nginx'],
 		path => '/var/www/puppet.png',
-    source => 'puppet:///modules/nginx/puppet.png',
-  }
+    	source => 'puppet:///modules/nginx/puppet.png',
+  	}
 
-  file { 'nginx.conf':
-    ensure => file,
+	file { 'nginx.conf':
+    	ensure => file,
 		path => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
+    	source => 'puppet:///modules/nginx/nginx.conf',
+    	require => Package['nginx'],
+	}
 
 	file { 'default.conf':
-    ensure => file,
+ 		ensure => file,
 		path => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
+    	source => 'puppet:///modules/nginx/nginx.conf',
+    	require => Package['nginx'],
+	}
 	
-	file { '/etc/nginx/conf.d/default.conf'
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File['nginx.conf', 'default.conf', 'puppet.png'],
-  }
-  
+	service { 'nginx':
+    	ensure => running,
+    	enable => true,
+ 		subscribe => File['nginx.conf', 'default.conf', 'puppet.png'],
+  	}
 }
