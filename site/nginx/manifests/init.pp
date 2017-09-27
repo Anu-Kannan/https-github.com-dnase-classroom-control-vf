5659757a3aa3cf4f941ecc27c5a143364cc0835d
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 12:57:48 -0400

    HW8.4
    add File

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,26 +1,24 @@
 class nginx {
+  File{
+      owner => 'root',
+    group => 'root',
+  }
   package { 'nginx':
     ensure => present,
   }
   
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
     mode => '0775',
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
     mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
     mode => '0664',
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
@@ -28,8 +26,6 @@ class nginx {
   }
   file { 'default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
     mode => '0664',
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
