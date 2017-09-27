c87cba3487da218b72838aeca627a36a8ee4cd62
Parker Wagnon <parkerw262@gmail.com>
2017-09-27 13:36:21 -0500

    update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,66 @@
 class nginx {
+    case $facts['os']['family'] {
+    'redhat','debian' : {
+        $package = 'nginx'
+        $owner = 'root'
+        $group = 'root'
+        $docroot = '/var/www'
+        $confdir = '/etc/nginx'
+        $logdir = '/var/log/nginx'
+}
+'windows' : {
+        $package = 'nginx-service'
+        $owner = 'Administrator'
+        $group = 'Administrators'
+        $docroot = 'C:/ProgramData/nginx/html'
+        $confdir = 'C:/ProgramData/nginx'
+        $logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+    fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+        'redhat' => 'nginx',
+        'debian' => 'www-data',
+        'windows' => 'nobody',
+        }
 File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    }
-  
-  package { 'nginx':
+    owner => $owner,
+    group => $group,
+    mode => '0664',
+}
+    package { $package:
     ensure => present,
-  }
-  
-  file { '/var/www':
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
-  }
-  file { '/var/www/index.html':
+}
+file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
-
-  }
-  
-  file { 'nginx.conf':
+}
+file { "${confdir}/nginx.conf":
     ensure => file,
-    path => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-   
-  }
-  file { 'default.conf':
+    content => epp('nginx/nginx.conf.epp',
+{
+    user => $user,
+    confdir => $confdir,
+    logdir => $logdir,
+}),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
     ensure => file,
-    path => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    
-  }
-  
-  service { 'nginx':
+    content => epp('nginx/default.conf.epp',
+{
+docroot => $docroot,
+}),
+notify => Service['nginx'],
+}
+    service { 'nginx':
     ensure => running,
     enable => true,
-  }
-  if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
-notify { "This is a ${vmname} virtual machine.": }
 }
-
 }
