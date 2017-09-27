3333872f16f9756c12cb4475481d50793e922b72
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 15:12:31 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,25 +2,25 @@ class nginx {
 $service = 'nginx'
 $port = 80
 case $facts['os']['family'] {
-'redhat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-}
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-}
-default : {
-fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-}
+  'redhat','debian' : {
+    $package = 'nginx'
+    $owner = 'root'
+    $group = 'root'
+    $docroot = '/var/www'
+    $confdir = '/etc/nginx'
+    $logdir = '/var/log/nginx'
+  }
+  'windows' : {
+    $package = 'nginx-service'
+    $owner = 'Administrator'
+    $group = 'Administrators'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $confdir = 'C:/ProgramData/nginx'
+    $logdir = 'C:/ProgramData/nginx/logs'
+  }
+  default : {
+    fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+  }
 }
 # user the service will run as. Used in the nginx.conf.epp template
 $user = $facts['os']['family'] ? {
