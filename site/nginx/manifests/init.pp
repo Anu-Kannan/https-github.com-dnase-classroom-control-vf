193ebfd66e12df0114f66b17347e011d5e863ceb
tracy18 <32303784+tracy18@users.noreply.github.com>
2017-09-27 15:05:05 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,55 +1,69 @@
 class nginx {
+  $service = 'nginx'
+  $port = '80'
   case $::osfamily {
-     'RedHat', 'Debian': {
-     $package = 'nginx'
-     $owner = 'root'
-     $group = 'root'
-     $docroot = '/var/www'
-     $confdir = '/etc/nginx'
-     $blockdir = '/etc/nginx/conf.d'
-     $logdir = '/var/log/nginx'
-     $service = 'nginx'
+    'RedHat', 'Debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
     }
     'windows': {
-     $package = 'nginx-service'
-     $owner = 'Administrator'
-     $group = 'Administrators'
-     $docroot = 'C:/ProgramData/nginx/html'
-     $confdir = 'C:/ProgramData/nginx'
-File {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+  }
+  $user = $::osfamily ? {
+    'Debian' => 'www-data',
+    'windows' => 'nobody',
+    default => 'nginx',
+  }
+  File {
     owner => 'root',
     group => 'root',
     mode => '0644',
-}
-package { 'nginx': 
-ensure => present,
   }
-file { '/var/www': 
-ensure => directory,
- }
-file { '/var/www/index.html':
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { 'nginx.conf':
-ensure => file,
-path => '/etc/nginx/nginx.conf',
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-}
-file { '/etc/nginx/conf.d':
-ensure => directory,
- }
-file { 'default.conf':
-ensure => file,
-path => '/etc/nginx/conf.d/default.conf',
-source => 'puppet:///modules/nginx/default.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-subscribe => File['nginx.conf', 'default.conf'],
-}
+  package { 'nginx':
+    ensure => present,
+  }  
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp', {
+      nginx_user => $user,
+      logdir => $logdir,
+      confdir => $confdir,
+      blockdir => $blockdir,
+    }),
+    require => Package['nginx'],
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    content => epp('nginx/default.conf.epp', {
+      port => $port,
+      docroot => $docroot,
+    }),
+    require => Package['nginx'],
+  }  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
+  }
 }
