9f9119484d9ef5879b62a6ecefd3910bfe962e5e
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 12:50:12 -0400

    Ex12.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  if $::virtual != 'physical' {
+     $vmname = capitalize($::virtual)
+     notify { "This is a ${vmname} virtual machine.": }
+}
 }
