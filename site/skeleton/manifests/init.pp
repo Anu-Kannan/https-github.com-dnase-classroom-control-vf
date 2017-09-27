d9c4fe8d5344408bbb605fa7b2bac0c4b9202c77
desjardy <sigatapoui@gmail.com>
2017-09-27 10:26:43 -0400

    indentation

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,13 +1,13 @@
 class skeleton {
-file {'/etc/skel':
-  ensure => directory,
-}
+  file {'/etc/skel':
+    ensure => directory,
+  }
 
-file {'/etc/skel/.bashrc':
-  ensure => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0440',
-  source  => 'puppet:///modules/skeleton/bashrc',
-}
+  file {'/etc/skel/.bashrc':
+    ensure => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    source  => 'puppet:///modules/skeleton/bashrc',
+  }
 }
