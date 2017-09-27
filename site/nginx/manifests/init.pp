97c06b39a942deafbc3b95aeb1f1b09c764b0f4f
Greg Jewett <greg@ejewett.com>
2017-09-27 14:08:31 -0500

    homework 13.2 update

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,36 @@
 class nginx {
 
+	$service = 'nginx'
+	$port = '80'
+	
+
+	case $facts['osfamily'] {
+		'Redhat', 'Debian': {
+			$package = 'nginx'
+			$owner = 'root'
+			$group = 'root'
+			$docRoot = '/var/www'
+			$configDir = '/etc/nginx'
+			$blockDir = '/etc/nginx/conf.d'
+			$logDir = '/var/log/nginx'
+		}
+		'windows': {
+			$winPath = 'C:/ProgramData/nginx'
+			$package = 'nginx-server'
+			$owner = 'Administrator'
+			$group = 'Administrator'
+			$docRoot = "${winPath}/html"
+			$configDir = "${winPath}"
+			$blockDir = "${winPath}/conf.d"
+			$logDir = "${winPath}/logs"
+		}
+	}
+	$nginxUser = $facts['osfamily'] ? {
+		'Debian' => 'www-data',
+		'windows' => 'nobody',
+		default => 'nginx',
+	}
+
 	File {
 		owner => 'root',
 		group => 'root',
@@ -22,14 +53,22 @@ class nginx {
 	file { 'nginx.conf':
 		ensure => file,
 		path => '/etc/nginx/nginx.conf',
-		source => 'puppet:///modules/nginx/nginx.conf',
+		content => epp('nginx/nginx.conf.epp', {
+			nginxuser => $user,
+			logDir => $logdir,
+			configDir => $confdir,
+			blockDir => $blockdir,
+		}),
 		require => Package['nginx'],
 	}
 	
 	file { 'default.conf':
 		ensure => file,
 		path => '/etc/nginx/conf.d/default.conf',
-		source => 'puppet:///modules/nginx/default.conf',
+		content => epp('nginx/default.conf.epp', {
+			port => $port,
+			docRoot => $docRoot,			
+		}),
 		require => Package['nginx'],
 	}
 	
