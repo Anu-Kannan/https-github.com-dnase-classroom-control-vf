c47b749c45f3c3f0c5128c896211b8d59c0cea46
Anu-Kannan <32294303+Anu-Kannan@users.noreply.github.com>
2017-09-27 10:02:20 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,31 +1,34 @@
-class nginx{
-package{ 'nginx':
-   ensure=>present,
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
+  }
 }
-file{'/var/www':
-   ensure => directory,
-}
-file{'/var/www/index.html':
-   ensure=>file,
-   source=>'puppet:///modules/nginx/index.html',
-}
-file{'nginx.conf':
-   ensure=>file,
-   path=>'/etc/nginx/nginx.conf'
-   source=>'puppet:///modules/nginx/nginx.conf',
-   require => Package['nginx'],
-   notify => Service['nginx'],
-}
-file{ 'default.conf':
-   ensure=>file,
-   path=>'/etc/nginx/conf.d/default.conf'
-   source=>'puppet:///modules/nginx/default.conf',
-   require => Package['nginx'],
-   notify => Service['nginx'],
-}
-service { 'nginx':
-   ensure => running,
-   enable => true,
-   subscribe=> File['nginx.conf','default.conf'],
-}   
-}
+
+
