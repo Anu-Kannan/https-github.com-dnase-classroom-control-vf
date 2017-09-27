99ef446c5b7bdc6ecb68ba0e80ea78292eb96e8d
tracy18 <32303784+tracy18@users.noreply.github.com>
2017-09-27 10:38:29 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,15 @@
+class memcached {
+package { 'memcached':
+ensure => present,
+}
+file { '/etc/sysconfig/memcached':
+    ensure => file,
+    content => "PORT='11211'\nUSER='memcached'\nMAXCONN='96'\nCACHESIZE='32'\nOPTIONS=''",
+    require => Package['memcached'],
+}
+service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+}
+}
