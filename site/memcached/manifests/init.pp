3a1b06063e42be4213a831159d2ba16c91a71d18
desjardy <sigatapoui@gmail.com>
2017-09-27 10:20:29 -0400

    updates memcached

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -9,7 +9,7 @@ file { '/etc/sysconfig/memcached':
   group   => 'root',
   mode    => '0644',
   source  => 'puppet:///modules/memcached/memcached',
-  require => Package['ntp'],
+  require => Package['memcached'],
 }
 
 service { 'memcached':
