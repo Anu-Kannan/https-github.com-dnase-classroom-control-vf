47573b9213d5c89465688647a5390a1477c27d6b
drewvas <32267047+drewvas@users.noreply.github.com>
2017-09-27 10:57:42 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,7 +2,7 @@ class memcached {
 package {'memcached':
 ensure => present,
 }
-file {'etc/sysconfig/memcached':
+file {'/etc/sysconfig/memcached':
 ensure =>file,
 content => "PORT='11211'\nUSER='memcached'\nMAXCONN='96'\nCACHESIZE='32'\nOPTIONS=''\n",
 require => Package['memcached'], 
