38955dc1f98cd6d48ba7d6dc698d8935525875d0
Greg Jewett <greg@ejewett.com>
2017-09-27 08:22:37 -0500

    homework 8.4

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,37 +3,47 @@
 class nginx {
   package { 'nginx':
     ensure => present, }
-    file { '/etc/nginx/nginx.conf':
+    #file { '/etc/nginx/nginx.conf':
   }
   
-  file { "/var/www":
+  file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
   }
   
-  file { "/var/www/index.html":
+  file { "index.html":
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => Package['nginx'],
+		path => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
-  }
-  
-  file { "/var/www/nginx.conf":
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     require => Package['nginx'],
-    source => 'puppet:///modules/nginx/nginx.conf',
   }
+
+  file { 'puppet.png':
+    ensure => file,
+    require => Package['nginx'],
+		path => '/var/www/puppet.png',
+    source => 'puppet:///modules/nginx/puppet.png',
+  }
+
+  file { 'nginx.conf':
+    ensure => file,
+		path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+	file { 'default.conf':
+    ensure => file,
+		path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+	
+	file { '/etc/nginx/conf.d/default.conf'
 
   service { 'nginx':
     ensure => running,
     enable => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
+    subscribe => File['nginx.conf', 'default.conf', 'puppet.png'],
   }
   
 }
