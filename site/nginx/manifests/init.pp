dd8ac601f57ee03f16cc6dc5f5b99c6e20ce5204
jostomlin <32303747+jostomlin@users.noreply.github.com>
2017-09-27 10:00:52 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -22,7 +22,6 @@ class nginx {
   service { 'nginx':
     ensure => running,
     enable => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
-    subscribe => File['/etc/nginx/conf.d/default.conf'],
+    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
   }
 }
