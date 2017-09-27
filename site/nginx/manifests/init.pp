f7abe1cfdcf36bcfab66417564f29858e44131ee
drewvas <32267047+drewvas@users.noreply.github.com>
2017-09-27 15:33:33 -0400

    HW 13.3

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,31 @@
 class nginx {
+$service = 'nginx'  
+$port = '80'  
+case $::osfamily {    
+'RedHat', 
+'Debian': {      
+$package = 'nginx'      
+$owner = 'root'      
+$group = 'root'      
+$docroot = '/var/www'       
+$confdir = '/etc/nginx'       
+$blockdir = '/etc/nginx/conf.d'       
+$logdir = '/var/log/nginx'     
+}     'windows': {       
+$package = 'nginx-service'      
+$owner = 'Administrator'       
+$group = 'Administrators'       
+$docroot = 'C:/ProgramData/nginx/html'      
+$confdir = 'C:/ProgramData/nginx'      
+$blockdir = 'C:/ProgramData/nginx/conf.d'      
+$logdir = 'C:/ProgramData/nginx/logs'    
+}
+}   
+$user = $::osfamily ? {     
+'Debian' => 'www-data',     
+'windows' => 'nobody',     
+default => 'nginx',   
+}
 File {
 owner => 'root',    
 group => 'root',    
@@ -16,7 +43,12 @@ file {'/var/www/index.html':
 }
 file {'/etc/nginx/nginx.conf':
   ensure => file,
-  source => 'puppet:///modules/nginx/nginx.conf',
+  content => epp('nginx/nginx.conf.epp', {       
+  nginx_user => $user,       
+  logdir => $logdir,       
+  confdir => $confdir,       
+  blockdir => $blockdir,  
+  }),
   require => Package['nginx'],
   notify => Service['nginx'],
 }
@@ -25,12 +57,16 @@ file { '/etc/nginx/conf.d':
   }
 file { '/etc/nginx/conf.d/default.conf':
   ensure => file,
-  source => 'puppet:///modules/nginx/default.conf',
+  content => epp('nginx/default.conf.epp', {       
+  port => $port,       
+  docroot => $docroot,     
+  }),
   require => Package['nginx'],
   notify => Service['nginx'],
 }
 service { 'nginx':
   ensure => running,
   enable => true,
+  subscribe => File['nginx.conf', 'default.conf'],
  }
 }
