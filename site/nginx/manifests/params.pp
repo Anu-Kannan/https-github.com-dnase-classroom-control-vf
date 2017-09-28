1348aa637a126066cea84339d1f973219700e41d
Greg Jewett <greg@ejewett.com>
2017-09-28 10:35:08 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,6 +1,28 @@
+	class nginx:params {
+
+	$service = 'nginx'
+	$port = '80'
 
 
-
-
-
-inherits apache::params
+	case $facts['osfamily'] {
+		'Redhat', 'Debian': {
+			$package = 'nginx'
+			$owner = 'root'
+			$group = 'root'
+			$deafultDocRoot = '/var/www'
+			$configDir = '/etc/nginx'
+			$serverBlock = '/etc/nginx/conf.d'
+			$logDir = '/var/log/nginx'
+		}
+		'windows': {
+			$winPath = 'C:/ProgramData/nginx'
+			$package = 'nginx-server'
+			$owner = 'Administrator'
+			$group = 'Administrator'
+			$deafultDocRoot = "${winPath}/html"
+			$configDir = "${winPath}"
+			$serverBlock = "${winPath}/conf.d"
+			$logDir = "${winPath}/logs"
+		}
+	}
+}
