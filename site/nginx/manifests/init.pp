723e144af2b5f57f6b414917e887d7313ab8e8fd
mtrog <Mathew.Rogers1@gmail.com>
2017-09-28 08:06:53 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,10 +1,12 @@
-class nginx {
+class nginx (
+  Optional[String] $root = undef,
+) {
   case $facts['os']['family'] {
     'redhat','debian' : {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      $default_docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
@@ -12,7 +14,7 @@ class nginx {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      $default_docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
@@ -26,6 +28,8 @@ class nginx {
     'debian' => 'www-data',
     'windows' => 'nobody',
   }
+  
+  $docroot = pick($root, $default_docroot)
   
   File {
     owner => $owner,
