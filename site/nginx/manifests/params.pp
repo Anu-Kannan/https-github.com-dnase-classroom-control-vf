f638fabbf7720baf1841d3f2ad60588cc0d60acd
desjardy <sigatapoui@gmail.com>
2017-09-28 11:33:34 -0400

    adds params class pattern

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,34 @@
+class nginx::params {
+  $source = 'puppet:///modules/nginx/'
+  $service  = 'nginx'
+  $port = '80'
+  
+  case $::osfamily {
+    'RedHat', 'Debian': { 
+      $packname = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $default_docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $servdir  = '/etc/nginx/conf.d'
+      $logsdir  = '/var/log/nginx'
+    } 
+    'windows'         : {
+      $packname = 'nginx-service'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $default_docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $servdir  = 'C:/ProgramData/nginx/conf.d'
+      $logsdir  = 'C:/ProgramData/nginx/logs'
+    } # apply the Windows class
+  }
+  
+  $docroot = pick($root,$default_root)
+  
+  $runas    = $::osfamily ? {
+    'Debian' => 'www-data',
+    'windows' => 'nobody',
+    default => 'nginx',
+  }
+}
