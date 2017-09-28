3479c0f6be3e3a4e6b169908cf62ac754da05df3
lancedawkins <32281682+lancedawkins@users.noreply.github.com>
2017-09-28 09:54:11 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,31 +1,15 @@
-class nginx {
-  $service = 'nginx'
-  $port = '80'
-  case $::osfamily {
-    'RedHat', 'Debian': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
-      $logdir = '/var/log/nginx'
-    }
-    'windows': {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $blockdir = 'C:/ProgramData/nginx/conf.d'
-      $logdir = 'C:/ProgramData/nginx/logs'
-    }
-  }
-  $user = $::osfamily ? {
-    'Debian' => 'www-data',
-    'windows' => 'nobody',
-    default => 'nginx',
-  }
+class nginx (
+  String $service = $nginx::params::service,
+  String $port = $nginx::params::port,
+  String $package = $nginx::params::package,
+  String $owner = $nginx::params::owner,
+  String $group = $nginx::params::group,
+  String $docroot = $nginx::params::docroot,
+  String $confdir = $nginx::params::confdir,
+  String $blockdir = $nginx::params::blockdir,
+  String $logdir = $nginx::params::logdir,
+  String $user = $nginx::params::user,
+) inherits nginx::params {
   File {
     owner => 'root',
     group => 'root',
@@ -34,10 +18,10 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }  
-  file { '/var/www':
+  file { $docroot:
     ensure => directory,
   }
-  file { '/var/www/index.html':
+  file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
