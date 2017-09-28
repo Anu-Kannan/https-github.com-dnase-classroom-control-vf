585dc81177ba3acdfd55f5cbe0000fc9d3e58353
lancedawkins <32281682+lancedawkins@users.noreply.github.com>
2017-09-28 06:27:30 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,18 +1,31 @@
 class nginx {
-  case $facts['os']['family'] {
-    'redhat','debian' : { 
-    $package = 'nginx'
-    $owner = 'root'
-    $group = 'root'
-    $docroot = '/var/www' $confdir = '/etc/nginx' $logdir = '/var/log/nginx'
+  $service = 'nginx'
+  $port = '80'
+  case $::osfamily {
+    'RedHat', 'Debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
   }
-  default : {
-    fail("Module ${module_name} is not supported on ${facts['os']['family']}") }
-}
-  $user = $facts['os']['family'] ? { 
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
-}
+  $user = $::osfamily ? {
+    'Debian' => 'www-data',
+    'windows' => 'nobody',
+    default => 'nginx',
+  }
   File {
     owner => 'root',
     group => 'root',
@@ -21,33 +34,36 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }  
-  file { [$docroot, "${confdir}/conf.d" ]:
+  file { '/var/www':
     ensure => directory,
   }
-  file { '${docroot}/index.html':
+  file { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { '${confdir}/nginx.conf':
-   ensure => file,
-   content => epp('nginx/nginx.conf.epp',
-    {
-      user => $user, 
-      confdir => $confdir, 
-      logdir => $logdir, 
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp', {
+      nginx_user => $user,
+      logdir => $logdir,
+      confdir => $confdir,
+      blockdir => $blockdir,
     }),
-    notify => Service['nginx'], 
-}
-  file { "${confdir}/conf.d/default.conf": 
-   ensure => file,
-   content => epp('nginx/default.conf.epp',
-    {
-      docroot => $docroot, 
+    require => Package['nginx'],
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    content => epp('nginx/default.conf.epp', {
+      port => $port,
+      docroot => $docroot,
     }),
-   notify => Service['nginx'],
+    require => Package['nginx'],
   }  
   service { 'nginx':
     ensure => running,
     enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
   }
 }
