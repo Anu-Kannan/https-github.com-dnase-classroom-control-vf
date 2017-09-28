8a6406938332e8c1ab1a7fc86bc306615cdfe490
mtrog <Mathew.Rogers1@gmail.com>
2017-09-28 08:41:27 -0700

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -4,7 +4,7 @@ class nginx::params {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $default_docroot = '/var/www'
+      $docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
@@ -12,7 +12,7 @@ class nginx::params {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $default_docroot = 'C:/ProgramData/nginx/html'
+      $docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
@@ -20,13 +20,9 @@ class nginx::params {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
     }
   }
-  
   $user = $facts['os']['family'] ? {
     'redhat' => 'nginx',
     'debian' => 'www-data',
     'windows' => 'nobody',
   }
-  
-  $docroot = pick($root, $default_docroot)
-
 }
