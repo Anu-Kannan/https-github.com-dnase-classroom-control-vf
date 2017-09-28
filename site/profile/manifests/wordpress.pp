c826d8844e7415994f4244bd600161034eca6e7f
CP50864 <32265266+CP50864@users.noreply.github.com>
2017-09-28 14:13:28 -0500

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,3 +1,18 @@
-class profile::wrodpress {
+class profile::wordpress {
+  class { 'mysql::server':
+    root_password => 'puppet4tehwin',
+  }
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
   
+  include apache
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port => '80',
+    docroot => '/opt/wordpress',
+    priority => '00',
+  }
+  
+  include ::wordpress
 }
