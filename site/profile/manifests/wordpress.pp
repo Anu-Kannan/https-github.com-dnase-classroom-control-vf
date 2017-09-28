43519b4042e37139b1f222ba8ca88bb8e75a6602
Greg Jewett <greg@ejewett.com>
2017-09-28 14:12:20 -0500

    capstone lab

---
+++ site/profile/manifests/wordpress.pp
@@ -0,0 +1,22 @@
+class profile::wordpress {
+	class { '::mysql::server':
+		root_password => 'puppet4thewin',
+	}
+	class { 'mysql::bindings':
+		php_enable => true
+	}
+	
+	include ::apache
+	include ::apache::mod::php
+	
+	apache::vhost { $facts['fqdn']:
+		port => '80',
+		docroot => '/opt/wordpress',
+		priority => '80',
+	}
+	
+	include ::wordpress
+	
+	
+	
+}
