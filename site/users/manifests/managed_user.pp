4bd06e61105366edce6d9ed4152f2d94baffd817
mtrog <Mathew.Rogers1@gmail.com>
2017-09-28 06:24:12 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,27 @@
+define users::managed_user (
+  $username = $title,
+  $homedir = "/home/${username}",
+  $groupname = $username,
+) {
+  File {
+    owner => $username,
+    group => $groupname,
+    mode => '0700',
+  }
+  
+  group { $groupname:
+    ensure => present,
+  }
+  
+  user { $username:
+    ensure => present,
+    gid => $groupname,
+    home => $homedir,
+  }
+  
+  file {
+    ensure => directory,
+  }
+  file { "${homedir}/.ssh":
+    ensure => directory,
+}
