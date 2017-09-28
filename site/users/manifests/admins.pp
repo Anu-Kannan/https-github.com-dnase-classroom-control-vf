eb907e2c2b0c0c19b9ed338ea1973b821960eb4f
Parker Wagnon <parkerw262@gmail.com>
2017-09-28 08:23:53 -0500

    create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,11 @@
+class users::admins {
+  users::managed_user { 'jim': }
+  users::managed_user { 'allen':
+  group => 'staff',
+}
+users::managed_user { 'parker':
+  group => 'staff',
+}
+group { 'staff':
+ensure => present,
+}
