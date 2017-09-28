4cab52caccae574557c2a39ede8a7717ad12cca0
lancedawkins <32281682+lancedawkins@users.noreply.github.com>
2017-09-28 09:53:35 -0700

    Create param.pp

---
+++ site/nginx/manifests/param.pp
@@ -0,0 +1,29 @@
+class nginx::params {
+  $service = 'nginx'
+  $port = '80'
+  case $::osfamily {
+    'RedHat', 'Debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+  }
+  $user = $::osfamily ? {
+    'Debian' => 'www-data',
+    'windows' => 'nobody',
+    default => 'nginx',
+  }
+}
