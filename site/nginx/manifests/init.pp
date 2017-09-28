29441be70b2c9b586ad137b36cca58cda9216ab9
desjardy <sigatapoui@gmail.com>
2017-09-28 11:35:04 -0400

    adds inherit params

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,7 @@
 class nginx (
   Optional[String] $root = undef,
-) {
-  $source = 'puppet:///modules/nginx/'
-  $service  = 'nginx'
-  $port = '80'
+) inherits nginx::params {
   
-  case $::osfamily {
-    'RedHat', 'Debian': { 
-      $packname = 'nginx'
-      $owner    = 'root'
-      $group    = 'root'
-      $default_docroot  = '/var/www'
-      $confdir  = '/etc/nginx'
-      $servdir  = '/etc/nginx/conf.d'
-      $logsdir  = '/var/log/nginx'
-    } 
-    'windows'         : {
-      $packname = 'nginx-service'
-      $owner    = 'Administrator'
-      $group    = 'Administrators'
-      $default_docroot  = 'C:/ProgramData/nginx/html'
-      $confdir  = 'C:/ProgramData/nginx'
-      $servdir  = 'C:/ProgramData/nginx/conf.d'
-      $logsdir  = 'C:/ProgramData/nginx/logs'
-    } # apply the Windows class
-  }
-  
-  $docroot = pick($root,$default_root)
-  
-  $runas    = $::osfamily ? {
-    'Debian' => 'www-data',
-    'windows' => 'nobody',
-    default => 'nginx',
-  }
-    
   File {
     owner   => $owner,
     group   => $group,
