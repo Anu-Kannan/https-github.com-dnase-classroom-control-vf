3cb2584b964320a01c20de1736d40bab53c70a9e
Drew Nase <dnasecs@gmail.com>
2017-09-28 11:37:30 -0400

    lab 17.2 - params.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,15 @@
 class nginx (
-  Optional[String] $root = undef,
-) {
-  $service = 'nginx'
-  $port = '80'
-  case $::osfamily {
-    'RedHat', 'Debian': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $default_docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
-      $logdir = '/var/log/nginx'
-    }
-    'windows': {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $blockdir = 'C:/ProgramData/nginx/conf.d'
-      $logdir = 'C:/ProgramData/nginx/logs'
-    }
-  }
-  $user = $::osfamily ? {
-    'Debian' => 'www-data',
-    'windows' => 'nobody',
-    default => 'nginx',
-  }
-  $docroot = pick($root, $default_docroot)
+  String $service = $nginx::params::service
+  String $port = $nginx::params::port
+  String $package = $nginx::params::package
+  String $owner = $nginx::params::owner
+  String $group = $nginx::params::group
+  String $docroot = $nginx::params::docroot
+  String $confdir = $nginx::params::confdir
+  String $blockdir = $nginx::params::blockdir
+  String $logdir = $nginx::params::logdir
+  String $user = $nginx::params::user
+) inherits nginx::params {
   File {
     owner => 'root',
     group => 'root',
