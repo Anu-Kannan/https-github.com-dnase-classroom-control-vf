88f664eccc9bf26830452803f701f1365576e720
tracy18 <32303784+tracy18@users.noreply.github.com>
2017-09-28 09:27:26 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,26 @@
-
+define users::managed_user (
+  $username = $title,
+  $groupname = $username,
+  $homedir = "/home/${username}",
+) {
+  File {
+    owner => $username,
+    group => $groupname,
+    mode => '0700',
+  }
+  group { $groupname:
+    ensure => present,
+  }
+  user { $username:
+    ensure => present,
+    gid => $groupname,
+    home => $homedir,
+    managehome => true,
+  }
+  file { $homedir:
+    ensure => directory,
+  }
+  file { "${homedir}/.ssh":
+    ensure => directory,
+  }
+}
