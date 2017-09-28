c4fe80eee72ec5ab45ccde6be51b0c1209e9609a
Drew Nase <dnasecs@gmail.com>
2017-09-28 14:57:57 -0400

    prep for capstone

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,10 +43,4 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  if $::is_virtual == true {
-    notify { "This is a ${capitalize($::virtual)} VM!": }
-  }
-  include nginx
-  include wrappers::epel
-  include wrappers::redis
 }
