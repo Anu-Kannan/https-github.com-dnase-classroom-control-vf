80105dd4d6374b7a2fb66adf7adb1eeae376fbb8
CP50864 <32265266+CP50864@users.noreply.github.com>
2017-09-28 10:28:21 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,72 +1,66 @@
-class nginx (
-  Optional[String] $root = undef,
-) {
-  $service = 'nginx'
-  $port = '80'
-  case $::osfamily {
-    'RedHat', 'Debian': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $default_docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
-      $logdir = '/var/log/nginx'
-    }
-    'windows': {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $blockdir = 'C:/ProgramData/nginx/conf.d'
-      $logdir = 'C:/ProgramData/nginx/logs'
-    }
-  }
-  $user = $::osfamily ? {
-    'Debian' => 'www-data',
-    'windows' => 'nobody',
-    default => 'nginx',
-  }
-  $docroot = pick($root, $default_docroot)
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-  }
-  package { 'nginx':
-    ensure => present,
-  }  
-  file { $docroot:
-    ensure => directory,
-  }
-  file { "${docroot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  file { 'nginx.conf':
-    ensure => file,
-    path => '/etc/nginx/nginx.conf',
-    content => epp('nginx/nginx.conf.epp', {
-      nginx_user => $user,
-      logdir => $logdir,
-      confdir => $confdir,
-      blockdir => $blockdir,
-    }),
-    require => Package['nginx'],
-  }
-  file { 'default.conf':
-    ensure => file,
-    path => '/etc/nginx/conf.d/default.conf',
-    content => epp('nginx/default.conf.epp', {
-      port => $port,
-      docroot => $docroot,
-    }),
-    require => Package['nginx'],
-  }  
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File['nginx.conf', 'default.conf'],
-  }
+class nginx {
+case $facts['os']['family'] {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
+}
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
+package { $package:
+ensure => present,
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
+ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+content => epp('nginx/default.conf.epp',
+{
+docroot => $docroot,
+}),
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
