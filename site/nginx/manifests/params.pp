13c4db45806213f8f5957fa73f470d0aca1350fa
mtrog <Mathew.Rogers1@gmail.com>
2017-09-28 10:15:11 -0700

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -4,6 +4,7 @@ class nginx::params {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
+      $docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
@@ -11,6 +12,7 @@ class nginx::params {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
