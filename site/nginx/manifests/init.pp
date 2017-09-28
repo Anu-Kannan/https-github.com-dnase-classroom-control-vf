7633d14925aeda364a6b6847fc65a40cbb657e6e
Greg Jewett <greg@ejewett.com>
2017-09-28 10:34:05 -0500

    lab 17.2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,31 +2,6 @@ class nginx (
 	Optional[String] $root = undef
 )
 {
-	$service = 'nginx'
-	$port = '80'
-	
-
-	case $facts['osfamily'] {
-		'Redhat', 'Debian': {
-			$package = 'nginx'
-			$owner = 'root'
-			$group = 'root'
-			$deafultDocRoot = '/var/www'
-			$configDir = '/etc/nginx'
-			$serverBlock = '/etc/nginx/conf.d'
-			$logDir = '/var/log/nginx'
-		}
-		'windows': {
-			$winPath = 'C:/ProgramData/nginx'
-			$package = 'nginx-server'
-			$owner = 'Administrator'
-			$group = 'Administrator'
-			$deafultDocRoot = "${winPath}/html"
-			$configDir = "${winPath}"
-			$serverBlock = "${winPath}/conf.d"
-			$logDir = "${winPath}/logs"
-		}
-	}
 	$nginxUser = $facts['osfamily'] ? {
 		'Debian' => 'www-data',
 		'windows' => 'nobody',
